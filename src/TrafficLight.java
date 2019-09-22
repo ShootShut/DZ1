12fb7514a93f3a161b7e5c11e7045bfb6085150b
Vladimir <shootshut@bk.ru>
2019-09-22 19:28:19 +0300

    Add source

--- src/TrafficLight.java
+++ src/TrafficLight.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class TrafficLight {
+    private String[] color = {"Green", "Yellow", "Red"};
+    private int[] time = new int[color.length];
+
+    public String[] getColor() {
+        return color;
+    }
+
+    public String getColor2(int x) {
+        return color[x];
+    }
+
+    public int getTime(int x) {
+        return time[x];
+    }
+
+    void initTime(int id, int time) {
+        this.time[id] = time;
+    }
+
+    void pause(int x) {
+        try {
+            Thread.sleep(x * 1000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
--- src/Work.java
+++ src/Work.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
 public class Work {
+    TrafficLight trafficLight;
+
+    void init() {
+        trafficLight = new TrafficLight();
+        for (int i = 0; i < trafficLight.getColor().length; i++) {
+            trafficLight.initTime(i, myPane());
+        }
+    }
+
+    void start() {
+        for (int i = 0; i < trafficLight.getColor().length; i++) {
+            System.out.println(trafficLight.getColor2(i));
+            trafficLight.pause(trafficLight.getTime(i));
+        }
+    }
+
+    public int myPane() {
+        String in = JOptionPane.showInputDialog("Введите количество минут");
+        int out;
+
+        try {
+            out = Integer.parseInt(in);
+//            return out;
+        } catch (Exception e) {
+            System.out.println(e);
+            out = -1;
+        }
+        return out;
+    }
 }
